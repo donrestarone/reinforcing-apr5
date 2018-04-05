b9b5e30cd9f59a2835c119ed18b5bdc539649967
donrestarone <shashikejayatunge@gmail.com>
2018-04-05 14:39:52 -0400

    finish exercise

--- data-events.rb
+++ data-events.rb
@@ -4,30 +4,46 @@ information = { data: { rooms:
     events: [
       { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
       { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
-      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 3, room_id: 1, start_time: 10, end_time: 18, attendees: 20 },
       { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
     ]
   }
 }
 
-def retreive_data(information)
-	information.each do |key, value|
-		information[key]
+room = information[:data]
+
+# puts room 
+# puts 
+# puts 
+
+rooms = room[:rooms]
+
+# puts rooms
+# puts rooms.class
+# capacity201 = rooms[0]
+
+# puts capacity201
+
+
+ def retreive_data(information)
+	information[:data][:events].each do |objects|  #looking into the hash argument and selecting key :data and selecting :events which is an array of hashes
+		if objects[:room_id] == 1  #to find events that are pointing to room 201
+			
+			if  objects[:attendees] > information[:data][:rooms][0][:capacity] #back out to the top level and go in the direction of room capacity
+				p "not enough space "
+			else
+				p "ok!"
+
+			end 
+
+		else 
+			p "not in room 201"
+		end 
+
 	end 
 
 end 
 
-room = information[:data]
+retreive_data(information)
 
-puts room 
-puts 
-puts 
-
-rooms = room[:rooms]
-
-puts rooms
-puts rooms.class
-capacity201 = rooms[0]
-
-puts capacity201
 
